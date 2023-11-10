86f3708f6d7500136e1a68439734ecd9b59f2a74
MrSinxx <sinaleylaz@yahoo.com>
2023-11-10 15:19:44 +0330

    feat:otp.py

---
+++ app/utils/otp.py
@@ -0,0 +1,24 @@
+from datetime import timedelta, datetime
+from fastapi import HTTPException, status
+import random
+
+from app.db.redis import RedisDB
+
+
+async def create_otp(id: str):
+    otp = str(random.randint(1000, 9999))
+    otp_expire = (datetime.utcnow() + timedelta(minutes=1)).strftime(
+        "%d/%m/%Y, %H:%M:%S"
+    )
+    redis = RedisDB()
+    await redis.set_data(key=otp, value=id, ex=60)
+    print(f"generated:{otp}  until:{otp_expire}")
+
+
+async def verify_otp(otp: str):
+    redis = RedisDB()
+    result = await redis.get_data(otp)
+    if result is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, "Invalid otp !!")
+    await redis.delete_data(otp)
+    return result
